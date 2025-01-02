288525870947c8955a8510c4bcee3b405caf250c
DanielRider <98618136+DanielRider@users.noreply.github.com>
2025-01-01 20:21:06 -0600

    slash commands

--- Discord_Integration/app.js
+++ Discord_Integration/app.js
@@ -1,2 +1,20 @@
 require('dotenv').config()
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const token = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}. I'M ZACH`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
 console.log("IM ZACH")

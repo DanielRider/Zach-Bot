dc5b8336152195634e69b2b5898df7de87a91ff7
DanielRider <riderDanielJ@gmail.com>
2025-01-05 12:04:37 -0600

    deploy commands, readme update~

--- Discord_Integration/commands/utility/zach-ping.js
+++ Discord_Integration/commands/utility/zach-ping.js
@@ -1,10 +1,9 @@
-t { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Zach!'); 
-
+        .setName('zach-ping')
+        .setDescription('Replies with Zach!'),
     async execute(interaction) {
         await interaction.reply("I'm Zach!!");
     },
